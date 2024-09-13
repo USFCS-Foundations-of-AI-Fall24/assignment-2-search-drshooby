8b2bda2eba24ecf88ff2a8c236aecfa5ab3aa0d0
David Shubov <drshubov@dons.usfca.edu>
2024-09-13 10:36:05 -0700

    sending in progress on marsplanner and search algorithms

--- mars_planner.py
+++ mars_planner.py
@@ -17,17 +17,25 @@ from copy import deepcopy
 from search_algorithms import breadth_first_search
 
 class RoverState :
-    def __init__(self, loc="station", sample_extracted=False, holding_sample=False, charged=False):
+    def __init__(self, loc="station", sample_extracted=False, holding_sample=False, holding_tool=False, sample_dropped_off=False, charged=False):
         self.loc = loc
-        self.sample_extracted=sample_extracted
+        self.sample_extracted = sample_extracted
         self.holding_sample = holding_sample
-        self.charged=charged
+        self.holding_tool = holding_tool
+        self.charged = charged
+        self.sample_dropped_off = sample_dropped_off
         self.prev = None
 
     ## you do this.
     def __eq__(self, other):
-       pass
-
+        return (
+            self.loc == other.loc and
+            self.sample_extracted == other.sample_extracted and
+            self.holding_sample == other.holding_sample and
+            self.holding_tool == other.holding_tool and
+            self.charged == other.charged and
+            self.sample_dropped_off == other.sample_dropped_off
+        )
 
     def __repr__(self):
         return (f"Location: {self.loc}\n" +
@@ -38,15 +46,24 @@ class RoverState :
     def __hash__(self):
         return self.__repr__().__hash__()
 
-    def successors(self, list_of_actions):
+    def successors(self, list_of_actions, depth=0, limit=0):
 
         ## apply each function in the list of actions to the current state to get
         ## a new state.
         ## add the name of the function also
-        succ = [(item(self), item.__name__) for item in list_of_actions]
-        ## remove actions that have no effect
+        if limit != 0:
+            if depth <= limit:
+                succ = [(item(self), item.__name__, depth) for item in list_of_actions]
+                ## remove actions that have no effect
 
-        succ = [item for item in succ if not item[0] == self]
+                succ = [item for item in succ if not item[0] == self]
+            else:
+                return []
+        else:
+            succ = [(item(self), item.__name__, depth) for item in list_of_actions]
+            ## remove actions that have no effect
+
+            succ = [item for item in succ if not item[0] == self]
         return succ
 
 ## our actions will be functions that return a new state.
@@ -56,6 +73,7 @@ def move_to_sample(state) :
     r2.loc = "sample"
     r2.prev=state
     return r2
+
 def move_to_station(state) :
     r2 = deepcopy(state)
     r2.loc = "station"
@@ -69,6 +87,27 @@ def move_to_battery(state) :
     return r2
 # add tool functions here
 
+def pick_up_tool(state):
+    r2 = deepcopy(state)
+    if not r2.holding_tool:
+        r2.holding_tool = True
+    r2.prev = state
+    return r2
+
+def drop_tool(state):
+    r2 = deepcopy(state)
+    if r2.holding_tool:
+        r2.holding_tool = False
+    r2.prev = state
+    return r2
+
+def use_tool(state):
+    r2 = deepcopy(state)
+    if r2.holding_tool:
+        r2.sample_extracted = True
+    r2.prev = state
+    return r2
+
 
 def pick_up_sample(state) :
     r2 = deepcopy(state)
@@ -81,6 +120,7 @@ def drop_sample(state) :
     r2 = deepcopy(state)
     if state.sample_extracted and state.loc == "station":
         r2.holding_sample = False
+        r2.sample_dropped_off = True
     r2.prev = state
     return r2
 
@@ -93,14 +133,21 @@ def charge(state) :
 
 
 action_list = [charge, drop_sample, pick_up_sample,
-               move_to_sample, move_to_battery, move_to_station]
+               move_to_sample, move_to_battery, move_to_station,
+               pick_up_tool, use_tool, drop_tool]
 
 def battery_goal(state) :
     return state.loc == "battery"
 ## add your goals here.
 
+def charge_goal(state) :
+    return state.charged
+
+def sample_goal(state) :
+    return state.sample_dropped_off
+
 def mission_complete(state) :
-    pass
+    return battery_goal(state) and charge_goal(state) and sample_goal(state)
 
 
 if __name__=="__main__" :
--- search_algorithms.py
+++ search_algorithms.py
@@ -6,13 +6,14 @@ from collections import deque
 def breadth_first_search(startState, action_list, goal_test, use_closed_list=True) :
     search_queue = deque()
     closed_list = {}
-
+    state_count = 0
     search_queue.append((startState,""))
     if use_closed_list :
         closed_list[startState] = True
     while len(search_queue) > 0 :
         ## this is a (state, "action") tuple
         next_state = search_queue.popleft()
+        state_count += 1
         if goal_test(next_state[0]):
             print("Goal found")
             print(next_state)
@@ -29,6 +30,7 @@ def breadth_first_search(startState, action_list, goal_test, use_closed_list=Tru
                 for s in successors :
                     closed_list[s[0]] = True
             search_queue.extend(successors)
+    print(state_count)
 
 ### Note the similarity to BFS - the only difference is the search queue
 
@@ -36,13 +38,14 @@ def breadth_first_search(startState, action_list, goal_test, use_closed_list=Tru
 def depth_first_search(startState, action_list, goal_test, use_closed_list=True,limit=0) :
     search_queue = deque()
     closed_list = {}
-
-    search_queue.append((startState,""))
+    state_count = 0
+    search_queue.append((startState,"", 0))
     if use_closed_list :
         closed_list[startState] = True
     while len(search_queue) > 0 :
-        ## this is a (state, "action") tuple
+        ## this is a (state, "action", depth) tuple
         next_state = search_queue.pop()
+        state_count += 1
         if goal_test(next_state[0]):
             print("Goal found")
             print(next_state)
@@ -52,16 +55,13 @@ def depth_first_search(startState, action_list, goal_test, use_closed_list=True,
                 print(ptr)
             return next_state
         else :
-            successors = next_state[0].successors(action_list)
+            successors = next_state[0].successors(action_list, next_state[2] + 1, limit)
             if use_closed_list :
                 successors = [item for item in successors
                                     if item[0] not in closed_list]
                 for s in successors :
                     closed_list[s[0]] = True
             search_queue.extend(successors)
+    print(state_count)
 
 ## add iterative deepening search here
-
-
-
-f
