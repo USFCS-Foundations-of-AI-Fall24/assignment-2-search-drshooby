91792309d8605f8cacddb140ee399e22d4dc354d
David Shubov <116600844+shooby-d@users.noreply.github.com>
2024-09-22 20:13:28 -0700

    adding submission.py

---
+++ Submission.py
@@ -0,0 +1,12 @@
+
+from search_algorithms import main as run_bfs_dfs_dls_submission
+from routefinder import main as a_star_submission
+from mapcoloring import main as antenna_submission
+
+if __name__ == '__main__':
+    print("\nRUNNING SEARCH ALGORITHM IN SUBMISSION.PY\n")
+    run_bfs_dfs_dls_submission()
+    print("\nRUNNING A* ALGORITHM IN SUBMISSION.PY\n")
+    a_star_submission()
+    print("\nRUNNING ANTENNA PROBLEM IN SUBMISSION.PY\n")
+    antenna_submission()
